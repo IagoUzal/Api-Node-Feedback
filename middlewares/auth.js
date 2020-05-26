6e187c5291e2cef2547580e4ede95e7e55f4872b
iago <iagouzal@gmail.com>
2020-05-26 15:01:49 +0200

    token change password

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,57 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-function userIsAuthenticated(req, res, next) {
+const { getConnection } = require('../db');
+const { generateError } = require('../helpers');
+
+async function userIsAuthenticated(req, res, next) {
   try {
     const { authorization } = req.headers;
 
-    const decoded = jwt.verify(authorization, process.env.SECRET);
+    if (!authorization) {
+      throw generateError('Falta la cabecera de authorization', 401);
+    }
 
-    next();
+    const authorizationParts = authorization.split(' ');
+
+    let token;
+
+    if (authorizationParts.length === 1) {
+      token = authorization;
+    } else if (authorizationParts[0] === 'Bearer') {
+      token = authorizationParts[1];
+    } else {
+      throw generateError('No puedo leer el token', 401);
+    }
+
+    let decoded;
+
+    decoded = jwt.verify(token, process.env.SECRET);
+
+    // Comprobar fecha de emision del token
+
+    const { id, iat } = decoded;
+
+    const connection = await getConnection();
+
+    const [result] = await connection.query(`select last_password_update from users where id=?`, [id]);
+
+    if (!result.length) {
+      throw generateError('El usuario no existe en la base de datos', 401);
+    }
+
+    const [user] = result;
+    console.log(user);
+    console.log(new Date(iat * 1000));
+    console.log(new Date(user.last_password_update));
+
+    if (new Date(iat * 1000) < new Date(user.last_password_update)) {
+      throw new Error('El token ya no vale, haz login para conseguir otro');
+    }
 
     req.auth = decoded;
+
+    next();
   } catch (error) {
     const authError = new Error('Invalid authorization');
     authError.httpCode = 401;
